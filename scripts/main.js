9ebd88c8ba804b5d1d89a5268b32eec941597052
JD Calvelli <jdcalvelli@gmail.com>
2021-10-23 00:09:22 -0500

    fix turn system by removing double action loop inside of enemy movement

--- scripts/main.js
+++ scripts/main.js
@@ -96,7 +96,8 @@ scene('main', ()=> {
     }
   ]);
 
-  //TURN ORDER CONTROLLER + movement (turn status needs to be checked every frame)  
+
+  //TURN ORDER CONTROLLER + movement (turn status needs to be checked every frame)
   if (turnStatus > 0) { //meaning it is the player's turn
     //allow player movement using cards
     //on click for each card, execute card action, update card value, update card sprite
@@ -121,12 +122,14 @@ scene('main', ()=> {
       card3Actual.use(sprite(card3ActualSprite));
       changeTurn();
     });
-  } else if (turnStatus < 0) { //meaning it's enemy turn, doesn't run because turn status situaiton isnt checked every frame
-    //enemy movement
-    //enemy ai
-    //want the enemy to follow a set path, then if the player is within a certain
-    //number of blocks of them, it should go to them
-    action('enemy', () => {
+  }
+
+  action ('enemy', () => {
+    if (turnStatus < 0) { //meaning it's enemy turn, doesn't run because turn status situaiton isnt checked every frame
+      //enemy movement
+      //enemy ai
+      //want the enemy to follow a set path, then if the player is within a certain
+      //number of blocks of them, it should go to them
       //checks to see if player is outside of 1 tile of enemy
       if (player.pos.x < enemy.pos.x - 32 || player.pos.x > enemy.pos.x + 32
         || player.pos.y < enemy.pos.y - 32 || player.pos.y > enemy.pos.y + 32) {
@@ -134,13 +137,11 @@ scene('main', ()=> {
             changeTurn();
       }
       else {
-        enemy.moveTo(player.pos.x, player.pos.y, moveAmount); //last arg is pixels per second
+        enemy.moveTo(player.pos.x, player.pos.y, 10000); //last arg is pixels per second
         changeTurn();
       }
-    });
-  } else {
-    console.log('something went seriously wrong lmao');
-  }
+    }
+  });
 
   //COLLISIONS
   //player collision
